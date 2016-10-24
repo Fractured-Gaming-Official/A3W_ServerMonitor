d8553d35227fcafd8900e3f7d3582868f51a8054
Mokey <McClainG9616@Gmail.com>
2016-10-23 23:48:55 -0400

    Added A3W_ServerMonitor

---
+++ Restart.bat
@@ -0,0 +1,38 @@
+@echo off
+:: rename all taskkill to proper exe file.
+taskkill /f /im "arma3server.exe" /t
+taskkill /f /im "arma3client.exe" /t 
+taskkill /f /im "servermonitor.bat" /t
+taskkill /f /im "servermonitor.exe" /t
+:: Server Params (Path to exe, noPause, noSound, noSplash, filePatching, autoInit, Profile Name, Path to Profile,Path to config file, Path to cfg, Mods, Battleye Path, Map, Server Port)
+start /wait /b C:\Games\Arma3\xxxxxx.exe -noPause -noSound -nosplash -filePatching -autoInit -name=default -profiles=C:\Games\Arma3\xxxxxx -config=C:\Games\Arma3\xxxxxx.cfg -cfg=C:\Games\Arma3\xxxxxx.cfg -mod=@CBA_A3 -BEPath=C:\Games\Arma3\xxBEfolder -world=Stratis -port=2302
+echo Server Initiated
+:bec
+taskkill /f /im  "bec.exe"
+timeout 10
+:: Path to BEC Folder
+cd "C:\Games\Arma3\A3Master\BEC\"
+:: Path to BEC.exe
+start "bec" "C:\Games\Arma3\A3Master\BEC\bec.exe" -f Config.cfg --dsc
+@echo Server Bec initiated!
+:: =================If running headless client, replace "HC .exe" with name of headless client .exe and remove "::"=================
+:: @echo Waiting to launch Headless Client
+:: timeout 30
+:: @echo Launching Headless Client
+::tasklist /fi "imagename eq arma3client.exe" |find ":" > nul
+:: if errorlevel 1 taskkill /f /im  "HC.exe"
+:: start /wait /b /high /affinity C0 C:\Games\Arma3\A3Master\arma3client.exe -client -connect=127.0.0.1 -port=2302 -profiles=C:\Games\Arma3\A3Master\Profiles -name=headlessclient -mod=@CBA_A3
+:monitor
+timeout 30 
+:: Rename "servermonitor.exe" to what ever you are doing to call the monitor system
+tasklist /fi "imagename eq servermonitor.exe" /fi "status eq running" |find  /i "servermonitor.exe"  >nul && ( 
+goto end
+) || ( 
+:: Path to Server Monitor
+start "servermonitor" "C:\Games\Arma3\A3Files\servermonitor.exe"
+)
+:end
+@echo Server Startup Complete
+@echo Closing Down
+timeout 10
+exit
---
+++ servermonitor.bat
@@ -0,0 +1,35 @@
+:: After editing this file, use the exe converter and convert it to a .exe file.
+@echo off
+:arma
+:: replace arma3server.exe with your server .exe name
+tasklist /fi "imagename eq arma3server.exe" /fi "status eq running" |find  /i "arma3server.exe"  >nul && ( 
+echo ------------------------------------------------
+echo Server is running will continue monitoring
+timeout 30
+goto bec
+) || ( 
+echo ------------------------------------------------
+echo Server is not running, will restart now
+:: Path to the restart.bat you edited
+start "Server" "C:\Games\Arma3\A3Master\BEC\restart.bat"
+echo Server startup initiated!
+exit
+)
+:bec
+:: shouldn't have to rename this unless BEC.exe is renamed, if so replace bec.exe with proper name
+tasklist /fi "imagename eq bec.exe" /fi "status eq running" |find  /i "bec.exe"  >nul && ( 
+echo ------------------------------------------------
+echo Server BEC is running will continue monitoring
+timeout 30
+goto arma
+) || ( 
+echo ------------------------------------------------
+echo Server BEC is not running, will restart now
+:: Path to BEC Folder
+cd C:\Games\Arma3\A3Master\BEC
+:: Path to BEC.exe with params
+start "Server BEC" "C:\Games\Arma3\A3Master\BEC\bec.exe" -f Config.cfg --dsc
+echo Server BEC startup initiated!
+timeout 30
+goto arma
+)
