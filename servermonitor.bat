bd9d833a845f8093ef814769d6371fa1058b7b37
Mokey <McClainG9616@Gmail.com>
2016-10-27 13:20:23 -0400

    Changed for friend

--- servermonitor.bat
+++ servermonitor.bat
@@ -6,30 +6,30 @@ tasklist /fi "imagename eq arma3server.exe" /fi "status eq running" |find  /i "a
 echo ------------------------------------------------
 echo Server is running will continue monitoring
 timeout 30
-goto bec
+goto arma
 ) || ( 
 echo ------------------------------------------------
 echo Server is not running, will restart now
 :: Path to the restart.bat you edited
-start "Server" "C:\Games\Arma3\A3Master\BEC\restart.bat"
+start "Server" "C:\Games\Arma3\A3Master\restart.bat"
 echo Server startup initiated!
 exit
 )
-:bec
+::bec
 :: shouldn't have to rename this unless BEC.exe is renamed, if so replace bec.exe with proper name
-tasklist /fi "imagename eq bec.exe" /fi "status eq running" |find  /i "bec.exe"  >nul && ( 
-echo ------------------------------------------------
-echo Server BEC is running will continue monitoring
-timeout 30
-goto arma
-) || ( 
-echo ------------------------------------------------
-echo Server BEC is not running, will restart now
+::tasklist /fi "imagename eq bec.exe" /fi "status eq running" |find  /i "bec.exe"  >nul && ( 
+::echo ------------------------------------------------
+::echo Server BEC is running will continue monitoring
+::timeout 30
+::goto arma
+::) || ( 
+::echo ------------------------------------------------
+::echo Server BEC is not running, will restart now
 :: Path to BEC Folder
-cd C:\Games\Arma3\A3Master\BEC
+::cd C:\Games\Arma3\A3Master\BEC
 :: Path to BEC.exe with params
-start "Server BEC" "C:\Games\Arma3\A3Master\BEC\bec.exe" -f Config.cfg --dsc
-echo Server BEC startup initiated!
-timeout 30
-goto arma
-)
+::start "Server BEC" "C:\Games\Arma3\A3Master\BEC\bec.exe" -f Config.cfg --dsc
+::echo Server BEC startup initiated!
+::timeout 30
+::goto arma
+::)
